a3004abd9042d5f570d87bba74b487ea10ea4b99
Koncept <68844962+Khynex@users.noreply.github.com>
2022-04-28 07:28:00 +0100

    Fist commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                'dark': '#F1f5f8 ',
+            }
+        },
+    },
+    plugins: [],
+}
